abb56aa19f0f12bdf42db8e8e967977b27c68e0f
Martin Brom <martinbrom97@gmail.com>
2021-06-04 22:39:27 +0200

    Fix Beealyzer showing the same menu to all players

--- src/main/java/cz/martinbrom/slimybees/items/bees/Beealyzer.java
+++ src/main/java/cz/martinbrom/slimybees/items/bees/Beealyzer.java
@@ -8,6 +8,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -31,14 +32,26 @@ public class Beealyzer extends SimpleSlimefunItem<ItemUseHandler> implements Rec
     private static final int[] ITEM_BORDER_SLOTS = { 3, 4, 5, 12, 14, 21, 22, 23 };
     private static final int ITEM_SLOT = 13;
 
-    private final ChestMenu menu;
     private final Map<UUID, BukkitRunnable> tickingMap = new HashMap<>();
 
     // TODO: 17.05.21 Add double setting for energy consumption
     public Beealyzer(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(category, item, recipeType, recipe);
+    }
 
-        menu = createMenu();
+    @Nonnull
+    @Override
+    public ItemUseHandler getItemHandler() {
+        // TODO: 18.05.21 Check if has charge
+        return e -> {
+            e.getInteractEvent().setCancelled(true);
+            createMenu().open(e.getPlayer());
+        };
+    }
+
+    @Override
+    public float getMaxItemCharge(ItemStack itemStack) {
+        return MAX_CHARGE_AMOUNT;
     }
 
     private ChestMenu createMenu() {
@@ -54,58 +67,44 @@ public class Beealyzer extends SimpleSlimefunItem<ItemUseHandler> implements Rec
             menu.addItem(slot, itemBorder, InvUtils.EMPTY_CLICK);
         }
 
-        menu.addMenuOpeningHandler(p -> {
-            BukkitRunnable prevRunnable = tickingMap.get(p.getUniqueId());
-            if (prevRunnable != null) {
-                tickingMap.remove(p.getUniqueId());
-            }
-
-            BukkitRunnable runnable = new BukkitRunnable() {
-                @Override
-                public void run() {
-                    if (menu.toInventory().getViewers().isEmpty()) {
-                        cancel();
-                    }
-
-                    Beealyzer.this.analyze();
-                }
-            };
-
-            runnable.runTaskTimer(plugin, 5L, 5L);
-
-            tickingMap.put(p.getUniqueId(), runnable);
-        });
-
         // TODO: 03.06.21 When closing menu add item inside back to inventory (if present)
-        menu.addMenuCloseHandler(p -> {
-            BukkitRunnable runnable = tickingMap.get(p.getUniqueId());
-            if (runnable != null) {
-                runnable.cancel();
-
-                tickingMap.remove(p.getUniqueId());
-            }
-        });
+        menu.addMenuOpeningHandler(p -> onMenuOpen(menu, p));
+        menu.addMenuCloseHandler(this::onMenuClose);
 
         return menu;
     }
 
-    @Nonnull
-    @Override
-    public ItemUseHandler getItemHandler() {
-        // TODO: 18.05.21 Check if has charge
-        return e -> {
-            e.getInteractEvent().setCancelled(true);
+    private void onMenuOpen(ChestMenu menu, Player p) {
+        BukkitRunnable prevRunnable = tickingMap.get(p.getUniqueId());
+        if (prevRunnable != null) {
+            tickingMap.remove(p.getUniqueId());
+        }
 
-            menu.open(e.getPlayer());
+        BukkitRunnable runnable = new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (menu.toInventory().getViewers().isEmpty()) {
+                    cancel();
+                }
+
+                Beealyzer.this.analyze(menu);
+            }
         };
+
+        runnable.runTaskTimer(SlimyBeesPlugin.instance(), 5L, 5L);
+        tickingMap.put(p.getUniqueId(), runnable);
     }
 
-    @Override
-    public float getMaxItemCharge(ItemStack itemStack) {
-        return MAX_CHARGE_AMOUNT;
+    private void onMenuClose(Player p) {
+        BukkitRunnable runnable = tickingMap.get(p.getUniqueId());
+        if (runnable != null) {
+            runnable.cancel();
+
+            tickingMap.remove(p.getUniqueId());
+        }
     }
 
-    protected void analyze() {
+    private void analyze(ChestMenu menu) {
         ItemStack item = menu.getItemInSlot(ITEM_SLOT);
 
         ItemStack analyzedItem = SlimyBeesPlugin.getBeeAnalysisService().analyze(item);
@@ -115,5 +114,6 @@ public class Beealyzer extends SimpleSlimefunItem<ItemUseHandler> implements Rec
             menu.addItem(ITEM_SLOT, analyzedItem);
         }
     }
+
 
 }
